2ce12833c5ee2ceaf0affc51ec2a25a2bd1a0e74
Anastasia_Liska <agni-0808@yandex.ru>
2022-11-05 01:30:41 +0300

    GetRandomArray

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -95,3 +95,15 @@ void ViewArray()
     int[] array = GetRandomArray(size, minValue, maxValue);
     Console.WriteLine($"[{String.Join(",", array)}]");
 }
+
+
+int[] GetRandomArray(int size, int minValue, int maxValue);
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue);
+    }
+
+    return result;
+}
