ff9a64a24cd548642eeebb57f6bd65adef578918
Anastasia_Liska <agni-0808@yandex.ru>
2022-11-05 00:25:40 +0300

    Pow

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -39,3 +39,21 @@ void MyPow()
 
     Console.WriteLine{$"{num} в степени {degree} - {Pow(num, degree)}"};
 }
+
+
+
+int Pow(int num, int rank)
+{
+    if (rank == 0) return 1;
+    else if (rank == 1) return num;
+
+    int result = num;
+
+    for (int i = 2; i <= rank; i++)
+    {
+        result *= num;
+    }
+
+    return result;
+}
+//end
